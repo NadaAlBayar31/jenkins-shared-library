29f39c95882f7c21e60f3071744a358a550e157a
NadaAlBayar31 <114536617+NadaAlBayar31@users.noreply.github.com>
2025-05-27 00:13:10 +0300

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,10 +1,3 @@
-def call(imageName) {
-    echo "Pushing Docker image ${imageName}"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-access', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
-        sh '''
-            echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
-        '''
-        sh "docker push ${imageName}"
-    }
+def call(String imageName) {
+    sh "docker build -t ${imageName} ."
 }
-
